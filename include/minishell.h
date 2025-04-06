2a4944c4a0a712c01a233250e26fc50aa0e2ce36
blohrer7 <benjamin-lohrer@web.de>
2025-04-06 12:05:14 +0200

    add structs

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: blohrer <blohrer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 08:49:49 by blohrer           #+#    #+#             */
-/*   Updated: 2025/04/02 10:13:08 by blohrer          ###   ########.fr       */
+/*   Updated: 2025/04/06 10:56:41 by blohrer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,62 +14,96 @@
 # define MINISHELL_H
 
 # include "../lib/ft_printf/ft_printf.h"
-# include "../lib/libft/libft.h"
 # include "../lib/gnl/get_next_line.h"
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdio.h>
-# include <string.h>
-# include <fcntl.h>
+# include "../lib/libft/libft.h"
 # include <dirent.h>
-# include <sys/wait.h>
-#include <sys/types.h>
-# include <limits.h>
 # include <errno.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <readline/history.h>
+# include <readline/readline.h>
 # include <signal.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
 
-
-typedef struct s_shell
+typedef struct s_main
 {
-	char	**envp;
+	char			**envp;
+	char			*unser_input;
+	char			*working_dir;
+	char			*old_working_dir;
+	pid_t			pid;
+	int				return_value;
+}					t_main;
 
-}	t_shell;
+typedef struct s_heredoc
+{
+	char			*delimiter;
+	bool			delimiter_quoted;
+	int				pipe_fd[2];
+	bool			pipe_open[2];
+	pid_t			pid;
+	int				status;
+}					t_heredoc;
 
+typedef enum s_type
+{
+	larger,
+	smaller,
+	d_larger,
+	d_smaller
+}					t_type;
 
+typedef struct s_metachar
+{
+	t_type			type;
+	bool			open_close;
+	int				fd;
+	bool			read_write;
+}					t_metachar;
 
-void	setup_signals(void);
-void	handle_sigint(int sig);
-void	handle_sigquit(int sig);
-void	init_terminal_settings(void);
-char	**tokenize_input(char *input);
-void	free_tokens(char **tokens);
-void	execute_command(char **tokens, t_shell *shell);
-char	*try_paths(char **paths, char *command);
-char	*resolve_path(char *command, char **envp);
-void	shell_free_split(char **arr);
-char	**get_paths_from_env(char **envp);
-void	exec_child(char *path, char **tokens, char **envp);
-int		is_builtin(char *cmd);
-int 	execute_builtin(char **tokens, t_shell *shell);
-int		ft_echo(char **tokens);
-int		ft_pwd(char **tokens);
-int		is_numeric(char *str);
-int		ft_exit(char **tokens);
-int		ft_cd(char **tokens);
-char	**copy_envp(char **envp);
-int		ft_env(char **tokens, char **envp);
-int		env_size(char **envp);
-void	remove_env_key(char ***envp, char *key);
-int		ft_unset(char **tokens, char ***envp);
-int		is_matching_key(char *env_var, char *key);
+typedef struct s_token
+{
+	char			*str;
+	int				type;
+	bool			quotes;
+	bool			var_exists;
+	struct t_token	*prev;
+	struct t_token	*next;
+	t_metachar		*meta;
+}					t_token;
 
+void				setup_signals(void);
+void				handle_sigint(int sig);
+void				handle_sigquit(int sig);
+void				init_terminal_settings(void);
+char				**tokenize_input(char *input);
+void				free_tokens(char **tokens);
+void				execute_command(char **tokens, t_main *shell);
+char				*try_paths(char **paths, char *command);
+char				*resolve_path(char *command, char **envp);
+void				shell_free_split(char **arr);
+char				**get_paths_from_env(char **envp);
+void				exec_child(char *path, char **tokens, char **envp);
+int					is_builtin(char *cmd);
+int					execute_builtin(char **tokens, t_main *shell);
+int					ft_echo(char **tokens);
+int					ft_pwd(char **tokens);
+int					is_numeric(char *str);
+int					ft_exit(char **tokens);
+int					ft_cd(char **tokens);
+char				**copy_envp(char **envp);
+int					ft_env(char **tokens, char **envp);
+int					env_size(char **envp);
+void				remove_env_key(char ***envp, char *key);
+int					ft_unset(char **tokens, char ***envp);
+int					is_matching_key(char *env_var, char *key);
 
-
-extern int g_exit_status;
-
-
-
+extern int			g_exit_status;
 
 #endif
