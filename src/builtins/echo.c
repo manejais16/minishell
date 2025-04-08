03e1e5ba82e9de82b97fddd52a9bd7b9159dcbf9
blohrer <you@example.com>
2025-04-08 08:46:48 +0200

    FIX: echo can handle now multiple  -n

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,32 +6,46 @@
 /*   By: blohrer <blohrer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 13:55:34 by blohrer           #+#    #+#             */
-/*   Updated: 2025/04/01 14:39:59 by blohrer          ###   ########.fr       */
+/*   Updated: 2025/04/08 08:40:46 by blohrer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	check_n_flags(char **tokens, int *i)
+{
+	int	j;
+	int	newline;
+
+	newline = 1;
+	while (tokens[*i] && tokens[*i][0] == '-')
+	{
+		j = 1;
+		while (tokens[*i][j] == 'n')
+			j++;
+		if (j == 1 || tokens[*i][j] != '\0')
+			break ;
+		newline = 0;
+		(*i)++;
+	}
+	return (newline);
+}
 
 int	ft_echo(char **tokens)
 {
 	int	i;
 	int	newline;
 
-	newline = 1;
 	i = 1;
-	if (tokens[1] && ft_strncmp(tokens[1], "-n", 3) == 0)
-	{
-		newline = 0;
-		i = 2;
-	}
+	newline = check_n_flags(tokens, &i);
 	while (tokens[i])
 	{
-		write(1, tokens[i], ft_strlen(tokens[i]));
+		ft_printf("%s", tokens[i]);
 		if (tokens[i + 1])
-			write(1, " ", 1);
+			ft_printf(" ");
 		i++;
 	}
 	if (newline)
-		write(1, "\n", 1);
+		ft_printf("\n");
 	return (0);
 }
