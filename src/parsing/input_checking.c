4aa490b0207b7b2cc8f8a2c9acbfeb83e7ed8db8
blohrer7 <benjamin-lohrer@web.de>
2025-04-26 18:26:59 +0200

    Fix: memory leak

--- src/parsing/input_checking.c
+++ src/parsing/input_checking.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   input_checking.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kzarins <kzarins@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: blohrer <blohrer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 19:25:31 by kzarins           #+#    #+#             */
-/*   Updated: 2025/04/23 15:44:39 by kzarins          ###   ########.fr       */
+/*   Updated: 2025/04/26 16:51:03 by blohrer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	is_metachar_combination_legal(t_token *first, t_token *second)
 {
 	if (*first->str == '|' && (*second->str == '<' || *second->str == '>'))
 		return (1);
-	if (is_meta_char(*first->str) && first->quote_type == NONE\
+	if (is_meta_char(*first->str) && first->quote_type == NONE
 		&& is_meta_char(*second->str) && second->quote_type == NONE)
 		return (0);
 	return (1);
@@ -38,6 +38,8 @@ int	check_for_repeating_meta(t_main *shell)
 	t_token	*temp;
 	t_token	*nextp;
 
+	if (!shell || !shell->first_token)
+		return (0);
 	temp = shell->first_token;
 	if (*temp->str == '|' && temp->quote_type == NONE)
 		return (print_tokenisation_error(shell, "|"), -1);
